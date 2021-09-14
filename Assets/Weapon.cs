3b7e95ef8702180f912cac19dfb8339e9ac757a0
murlord <ray12011994@gmail.com>
2021-09-14 18:41:01 -0400

    Lab8

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -7,7 +7,7 @@ public class Weapon : MonoBehaviour
     public float damage = 50f;
     public float range = 100f;
     public Camera fpsCam;
-    public ParticleSystem muzzleFlash;
+    
     public GameObject impactEffect;
     public float impactForce =30f;
     public float fireRate = 20f;
@@ -30,7 +30,7 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
-        muzzleFlash.Play();
+        
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
